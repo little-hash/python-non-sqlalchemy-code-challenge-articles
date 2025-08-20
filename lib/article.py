cdcae5d82454adb994780cf52af9ef672ea8487a
Ezra <ezraraddai166@gmail.com>
2025-08-20 13:45:58 +0300

    firstcommit~

--- lib/article.py
+++ lib/article.py
@@ -91,21 +91,21 @@ mag1 = Magazine("Writers Weekly", "Literature")
 mag2 = Magazine("Tech Today", "Technology")
 mag3 = Magazine("Global Voices", "Culture")
 
-# Create Article instances using `add_article`
+# Article instances using
 article1 = author1.add_article(mag1, "The Power of Storytelling")
 article2 = author2.add_article(mag1, "Identity and Expression")
 article3 = author2.add_article(mag2, "The Digital Divide")
 article4 = author3.add_article(mag3, "Bridging Cultures")
 article5 = author1.add_article(mag3, "Language and Belonging")
 
-# DEMONSTRATE Author Methods
+# Author Methods
 print("Author1 Articles:", author1.articles())
 print("Author1 Magazines:", author1.magazines())
 
 print("Author2 Articles:", author2.articles())
 print("Author2 Magazines:", author2.magazines())
 
-# DEMONSTRATE Magazine Methods
+# Magazine Methods
 print("Mag1 Articles:", mag1.articles())
 print("Mag1 Contributors:", mag1.contributors())
 print("Mag1 Titles:", mag1.article_titles())
